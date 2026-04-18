05ec470b91ad79e1adece4cb473ab33635b2ab83
Leandro <5417585+leanish@users.noreply.github.com>
2026-04-18 22:44:23 +0300

    prepare 0.4.0 release (#51)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "io.github.leanish"
-version = "0.4.0-SNAPSHOT"
+version = "0.4.0"
 description = "AWS SQS payload interceptor for automatic compression and encoding."
 
 val targetJavaVersion = 17
